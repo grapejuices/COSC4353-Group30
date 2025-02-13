17a214daae6b85687ce0f806dd5c12837e6a4eb6
JohnBunger <iamtheonlypro@gmail.com>
2025-02-13 03:37:09 -0600

    Update App.tsx
    added /Volunteer-History-Temp path for the site

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { LoginPage } from "./pages/LoginPage";
 import { SignUpPage } from "./pages/SignUpPage";
 import { EveManagement } from "./pages/EventManagmentPage";
 import { VProfilePage } from "./pages/VProfilePage";
+import { VolunteerHistoryTMP } from "./pages/VolunteerHistoryTMP";
 import { AuthProvider } from "./AuthProvider"; 
 import ProtectedRoute from "./ProtectedRoute"; 
 
@@ -34,6 +35,7 @@ function App() {
           />
           <Route path="/vdashboard" element={<p>Volunteer Dashboard.</p>} />
           <Route path="/adashboard" element={<p>Admin Dashboard.</p>} />
+          <Route path="/Volunteer-History-Temp" element={<p>Volunteer History.</p>} />
           {/* <Route path="/vdashboard" element={<ProtectedRoute allowedRoles="volunteer"><p>Volunteer Dashboard.</p></ProtectedRoute>} /> */}
           {/* <Route path="/adashboard" element={<ProtectedRoute allowedRoles="admin"><p>Admin Dashboard.</p></ProtectedRoute>} /> */}
         </Routes>
