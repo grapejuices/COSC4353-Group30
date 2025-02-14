983276d681c5ec5687b2c36191555a184e3055a3
JohnBunger <iamtheonlypro@gmail.com>
2025-02-14 04:43:41 -0600

    Volunteer History started
    just copied the code and got rid of some of what i dont need

--- client/src/pages/VolunteerHistoryTMP.tsx
+++ client/src/pages/VolunteerHistoryTMP.tsx
@@ -1,8 +1,22 @@
-import { LoginForm } from "@/components/LoginForm"
-export const LoginPage = () => {
+// import { EventSheet } from "@/components/EventForm";
+import { EventTable } from "@/components/EventTable";
+import { VolunteerEvent } from "@/lib/temporary_values";
+import { useState } from "react";
+
+export const VolunteerHistory = () => {
+  const [selectedEvent, setSelectedEvent] = useState<VolunteerEvent | null>(null);
+
+  const handleEditEvent = (event: VolunteerEvent) => {
+    setSelectedEvent(event);
+  };
+
+  const closeSheet = () => {
+    setSelectedEvent(null);
+  };
+
   return (
-    <div className="flex items-start justify-center min-h-auto bg-white">
-      <LoginForm />
+    <div>
+      <EventTable onEditEvent={handleEditEvent} />
     </div>
-  )
-}
+  );
+};
